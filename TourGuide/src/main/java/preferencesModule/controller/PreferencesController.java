e86e937dd11c23823695fa6100770c4238a84bf2
olivier.bonheur <olivier.bonheur@gmail.com>
2020-09-27 22:29:43 +0200

    Working on Controller

--- TourGuide/src/main/java/preferencesModule/controller/PreferencesController.java
+++ TourGuide/src/main/java/preferencesModule/controller/PreferencesController.java
@@ -1,5 +1,7 @@
 package preferencesModule.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,12 +14,16 @@ import java.util.UUID;
 
 @RestController
 public class PreferencesController {
+    private Logger logger = LoggerFactory.getLogger(PreferencesController.class);
 
     @Autowired
     IPreferencesService preferencesService;
 
     @RequestMapping("/getPrice")
-    public List<Provider> getPrice(@RequestParam String apiKey, @RequestParam UUID attractionId, @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints) {
-        return preferencesService.getPrice(apiKey, attractionId, adults, children, nightsStay, rewardsPoints);
+    public List<Provider> getPrice(@RequestParam String apiKey, @RequestParam String attractionId, @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints) {
+        logger.debug("Request getPrice : ApiKey=" + apiKey + " - attractionId=" + attractionId + " - adults=" + adults + " - children=" + children + " - nightsStay=" + nightsStay + " - rewardsPoints=" + rewardsPoints);
+        List<Provider> providers = preferencesService.getPrice(apiKey, UUID.fromString(attractionId), adults, children, nightsStay, rewardsPoints);
+        logger.debug("Response Nb Providers=" + providers.size());
+        return providers;
     }
 }
