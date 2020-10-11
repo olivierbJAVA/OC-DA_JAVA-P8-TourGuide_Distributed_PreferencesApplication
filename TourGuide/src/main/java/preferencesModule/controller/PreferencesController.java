ae51fa2a2871641d665565115327f4d3984291fb
olivier.bonheur <olivier.bonheur@gmail.com>
2020-10-11 15:45:40 +0200

    Work on PreferencesController

--- TourGuide/src/main/java/preferencesModule/controller/PreferencesController.java
+++ TourGuide/src/main/java/preferencesModule/controller/PreferencesController.java
@@ -2,7 +2,6 @@ package preferencesModule.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +15,11 @@ import java.util.UUID;
 public class PreferencesController {
     private Logger logger = LoggerFactory.getLogger(PreferencesController.class);
 
-    @Autowired
-    private IPreferencesService preferencesService;
+    private final IPreferencesService preferencesService;
+
+    public PreferencesController(IPreferencesService preferencesService) {
+        this.preferencesService = preferencesService;
+    }
 
     @GetMapping("/getPrice")
     public List<Provider> getPrice(@RequestParam String apiKey, @RequestParam String attractionId, @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints) {
